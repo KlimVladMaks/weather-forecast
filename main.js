922972b06198c9012c5ba027510170e43ef34d0c
KlimVladMaks <vkm.blog@yandex.ru>
2023-05-13 00:14:42 +0300

    Commit #8

--- main.js
+++ main.js
@@ -7,9 +7,69 @@ const form = document.querySelector("#form");
 // Получаем поле ввода, используемое для ввода названия города
 const input = document.querySelector("#inputCity");
 
+// Функция для удаления текущей карточки на странице
+function removeCard() {
+
+    // Находим и удаляем предыдущую карточку с погодой (если она не равна null)
+    const prevCard = document.querySelector(".card");
+    if (prevCard) prevCard.remove();
+}
+
+// Функция для отображения карточки с текстом ошибки
+function showError(errorMessage) {
+
+    // Формируем карточку с текстом ошибки
+    const html = `<div class="card">${errorMessage}</div>`
+
+    // Вставляем карточку с ошибкой после хэдера
+    header.insertAdjacentHTML("afterend", html);
+}
+
+// Функция для отображения карточки с погодой
+// (Функция получает на вход объект с данными и погоде и извлекает из него необходимые данные)
+function showCard({name, country, temp, condition}) {
+
+    // HTML-код карточки с погодой, заполненной в соответствии с полученными данными
+    const html = `
+    <div class="card">
+        <h2 class="card-city">${name} <span>${country}</span></h2>
+        <div class="card-weather">
+            <div class="card-value">${temp}<sup>°C</sup></div>
+            <img class="card-img" src="./img/example.png" alt="Weather">
+        </div>
+        <div class="card-description">${condition}</div>
+    </div>
+    `;
+
+    // Вставляем созданный HTML-код после хэдера
+    header.insertAdjacentHTML("afterend", html);
+}
+
+// Асинхронная функция для получения погоды для заданного города
+async function getWeather(city) {
+
+    // API-ключ для получения прогноза погоды
+    const apiKey = "a94e6b40c8304ad09cf214421231005"
+
+    // Стандартный запрос погоды с использованием API-ключа для заданного города
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+
+    // Отправляем запрос и получаем ответ
+    const response = await fetch(url);
+
+    // Преобразуем ответ в формат JSON
+    const data = await response.json();
+
+    // Выводим в консоль полученные данные
+    console.log(data);
+
+    // Возвращаем полученные данные
+    return data;
+}
+
 // Добавляем к форме функцию, которая будет срабатывать при отправке формы
 // (e - это событие, возникшее при отправке формы)
-form.onsubmit = function(e) {
+form.onsubmit = async function(e) {
 
     // Отменяем событие по-умолчанию при отправке формы
     e.preventDefault();
@@ -17,61 +77,36 @@ form.onsubmit = function(e) {
     // Извлекаем название города из поля ввода, удаляя начальные и конечные пробелы
     let city = input.value.trim();
 
-    // API-ключ для получения прогноза погоды
-    const apiKey = "a94e6b40c8304ad09cf214421231005"
+    // Получаем погодные данные для заданного города
+    const data = await getWeather(city);
 
-    // Стандартный запрос погоды с использованием API-ключа для заданного города
-    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+    // Если в данных указана ошибка
+    if (data.error) {
 
-    // Отправляем запрос и обрабатываем ответ (response)
-    fetch(url).then((response) => {
+        // Удаляем предыдущую карточку
+        removeCard();
 
-        // Возвращаем ответ, преобразуя его в формат JSON
-        return response.json();
+        // Отображаем карточку с сообщением об ошибке
+        showError(data.error.message);
+    }
 
-    // Обрабатываем полученные в формате JSON данные (data)
-    }).then((data) => {
+    // Иначе
+    else {
 
-        // Выводим в консоль полученные данные
-        console.log(data);
+        // Удаляем предыдущую карточку
+        removeCard();
 
-        // Если в данных указана ошибка
-        if (data.error) {
-
-            // Находим и удаляем предыдущую карточку с погодой (если она не равна null)
-            const prevCard = document.querySelector(".card");
-            if (prevCard) prevCard.remove();
-
-            // Формируем карточку с текстом ошибки
-            const html = `<div class="card">${data.error.message}</div>`
-
-            // Вставляем карточку с ошибкой после хэдера
-            header.insertAdjacentHTML("afterend", html);
+        // Создаём объект для хранения данных о погоде
+        const weatherData = {
+            name: data.location.name,
+            country: data.location.country,
+            temp: data.current.temp_c,
+            condition: data.current.condition.text
         }
 
-        // Иначе
-        else {
-
-            // Находим и удаляем предыдущую карточку с погодой (если она не равна null)
-            const prevCard = document.querySelector(".card");
-            if (prevCard) prevCard.remove();
-
-            // HTML-код карточки с погодой, заполненной в соответствии с полученными данными
-            const html = `
-            <div class="card">
-                <h2 class="card-city">${data.location.name} <span>${data.location.country}</span></h2>
-                <div class="card-weather">
-                    <div class="card-value">${data.current.temp_c}<sup>°C</sup></div>
-                    <img class="card-img" src="./img/example.png" alt="Weather">
-                </div>
-                <div class="card-description">${data.current.condition.text}</div>
-            </div>
-            `;
-
-            // Вставляем созданный HTML-код после хэдера
-            header.insertAdjacentHTML("afterend", html);
-        }
-    })
+        // Отображаем карточку с погодой, передавая ей данные о погоде
+        showCard(weatherData);
+    }
 }
 
 
